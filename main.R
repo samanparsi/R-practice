66611c735e2844b105683fed1436c5e1db7df505
samanparsi <samanparsi@outlook.com>
2022-07-13 21:07:45 +0430

    added first functions and instructions

--- main.R
+++ main.R
@@ -1,3 +1,14 @@
-Print ("This file was created within Rstudio")
+myfunction <- function() { #function deceleration#
+  
+  x <- rnorm(100)
+  mean(x)
+}
 
-print ("And now it lives on Github")
+second <- function(x){ #another function#
+  x + rnorm((length(x)))
+}
+
+#to use function in console first you have to introduce it
+#via  source("FILENAME")  .note that you should first save  the file.
+#or just copy paste it. Also every change requires new sourcing.
+#It is possible to source from multiple files via fallowing commands:

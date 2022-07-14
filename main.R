6cc891f56e73dae31dd3c14d09e384675f51b9be
samanparsi <samanparsi@outlook.com>
2022-07-14 09:50:16 +0430

    added data frames , elemnt naming , missing values , factors and vectors / lists

--- main.R
+++ main.R
@@ -37,3 +37,44 @@ second <- function(x){ #another function#
 #c functions can be used for creating vectors:
 x<- c(0.5,0.6)
 y<- c(TRUE,FALSE)
+z<- c(T,F)
+#if you put give to elements in this format you will get a vector converted in 
+#one of the formats. So be carefull there will be no errors.
+as.numeric(x) #changes x to a numeric function. other types are
+#allowed if possible
+# FALSE=0 TRUE=1
+
+L<- list(1,"a",TRUE,1+4i) #this is allowed
+
+#matrixs are vectors with dymentions. They are columns wise created in R
+m<- matrix(1:6,nrow=2, ncol=3)
+dim(m)
+
+#factors: representing categorical data. lm() , glm() 
+#they are discrete values and can
+#be ladder data (military ranks) or not(male,female)
+f<- factor(c("yes","yes","no","yes", "no"))
+table(f)
+unclass(f)
+#base on alphabet , by default you will get default level unless you declare it
+# by adding in in factor function
+
+#missing value: is.na() , is.nan() for mathematical values
+#NA values are having class , NAN is also a NA.
+na1<-c(1,2,NA,NaN)
+is.na(na1)
+is.nan(na1)
+
+#data frames: a formatted organized database.
+#they have special attr like row.names
+#read.table() , read.csv()
+#data.matrix() converts them into matrix when every thing is in same class
+myframe<- data.frame(foo=1:4,bar=c(T,T,F,F))
+
+#each element of vector can be named:
+#x<- 1:3    #names(x)<-c("foo","bar","norf")
+#x<- list(a=1,b=2,c=3) list also can get names 
+#names can be called by $a
+#matrix can have names for dimensions only 
+#dimnames(m)<-list(c("a","b"),c("c","d"))
+

cf96089b4afaabecc536409c56af54deb7197e66
samanparsi <samanparsi@outlook.com>
2022-07-15 18:27:43 +0430

    3 dot , lazy Eval and default args is being added

--- Functions.R
+++ Functions.R
@@ -20,6 +20,57 @@ columnmean<- function(y, removeNA= TRUE){
   means
 }
 
-#functions can have default values
+##functions can have default values##
+
 #just like python we can match the parsed arguments to suitable function
 #with the same name automatically 
+args(lm) #see defaults and required args
+#it is possible part of argument name , if there was no exact match it would 
+#search for partial match and positional match. note that the answer must be 
+#uniq
+
+f<- function (a,b=1,c=2,d=NULL){} #a has no default value, b and c not 
+
+#R has lazy evaluation. arguments are being checked if they are requeird
+f<-function(a,b){
+  a^2
+}
+f(2)
+#no error will be shown, we don't need it so R wouldn't check for it.
+f<-function(a,b){
+  print (a)
+  print (b)
+  
+}
+f(45)
+#this gives error because b is needed. note a is being printed.
+
+#the 3 dot argument ...
+#it's for extending a functions and creating a copy with some changes
+#this way it will copy other stuff and just changes replaced parts
+myplot<- function(x,y,type="1",...){
+  plot(x,y,type=type,...)
+}#just type is being set to "1" nothing else would be different
+#also we can use it to add things to generic functions.
+#Also we can use it if the number of arguments can not be known in advanced
+args(paste)
+args(cat)
+#this 2 take a lot of  input thus they have ... at first argument.
+
+
+#anything after ... MUST BE NAMED EXACTLY , no partial match for them.
+paste("a","b",sep=":")
+paste("a","b",se=":") #run this
+
+
+
+
+
+
+
+
+
+
+
+
+

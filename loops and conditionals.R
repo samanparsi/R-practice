0538e7266d4edb4a4b86c5df9c3da53d59054c9d
samanparsi <samanparsi@outlook.com>
2022-07-15 15:05:42 +0430

    renamed loops and conditionals in order to uses it as R program

---
+++ loops and conditionals.R
@@ -0,0 +1,13 @@
+x<-"saman"
+
+for (letter in x){
+  print(x[i])
+}
+#the format used in python allowed in here
+#we can uses nested for loops but its slow and hard to read
+
+y<-if(x==3){
+  5
+} elses{
+  0
+}

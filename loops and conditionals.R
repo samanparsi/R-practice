51029602695b353a0c72ec9bba30a02536534a99
samanparsi <samanparsi@outlook.com>
2022-07-15 18:11:56 +0430

    completed the explanations and finishing touches has been done

--- loops and conditionals.R
+++ loops and conditionals.R
@@ -1,13 +1,33 @@
 x<-"saman"
 
 for (letter in x){
-  print(x[i])
+  print(letter)
 }
 #the format used in python allowed in here
 #we can uses nested for loops but its slow and hard to read
 
 y<-if(x==3){
   5
-} elses{
+} else{
   0
 }
+
+for (i in 1:4) print (x[i]) #only if for has one statement
+
+###most of the code is similar to python, it is easy and agile to develop
+#use seq_leg() in order to create a progressing loop
+count<-0
+while (count<10){
+  print(count)
+  count<-count+1
+} #careful it can go to infinity LOL
+#####the condition's are being checked from LEFT to RIGHT !!!####
+#repeat{code} is going to always performed unless we call break 
+#I don't like repeat 
+for (i in 1:100){
+  if (i<=20){
+    #skip the first 02 iter
+    next
+  }
+}
+#above code is used to skip or control the loop using "next"

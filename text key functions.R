98cfc67d1cdb3c6cd057f785bd75cb58124592f2
samanparsi <samanparsi@outlook.com>
2022-07-28 18:59:23 +0430

    usefull function to handle text in R

---
+++ text key functions.R
@@ -0,0 +1,24 @@
+#toupper and to lower 
+
+#strsplit(data,"\\what to split with")
+
+#firstElement<-function(x){x[1]}
+#sapply(data,firstElement) 
+#get first element of each header and remove no alphabetrics
+
+#sub("_","",data,) remove all _ in data just the first one founded
+#gsub does the same for all the elements given not just first one 
+
+#grep("name",data$data) find name in data , value=TRUE returns the data when
+#name is found
+#grepl("name",data$data) returns a vector for each finding with true and false 
+#for not found
+
+#nchar("") gives number of chars
+
+#substr("",1,2) returns a part of string
+
+#paste("" , "") joins strings
+#paste0 does the same without any space between them
+
+#str_trim("") removes all the extra space from data
